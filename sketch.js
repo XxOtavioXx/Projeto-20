9921ee001261ab951574f31f4443d4d54b43f03a
XxOtavioXx <102555339+XxOtavioXx@users.noreply.github.com>
2022-07-06 12:58:38 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+ var plane_options={
+	isStatic: true
+} 
+var block1_options={
+	restitution:0.5,
+	friction:0.02,
+	frictionAir:0
+}
+var block2_options={
+	restitution:0.7,
+	friction:0.01,
+	frictionAir:0.1
+}
+var block3_options={
+	restitution:0.01,
+	friction:1,
+	frictionAir:0.3
+} 
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(450, 450);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Crie os Corpos Aqui.
+	plane= Bodies.rectangle(225,450,450,10,plane_options);
+	World.add (world, plane)
+	block1= Bodies.circle(220,10,20,block1_options);
+	World.add (world, block1)
+	block2= Bodies.rectangle(110,50,50,50,block2_options);
+	World.add (world, block2)
+	block3= Bodies.rectangle(350,50,100,60,block3_options);
+	World.add (world, block3)
+	
+
+	
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("green");
+  Engine.update(engine);
+  ellipse(block1.position.x,block1.position.y,40)
+  rect(plane.position.x,plane.position.y,450,10)
+  rect(block2.position.x,block2.position.y,50,50)
+  rect(block3.position.x,block3.position.y,100,60)
+  drawSprites();
+ 
+}
+
+
+
